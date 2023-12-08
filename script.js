e3b517bf8fb9bd1eed5353c0245e694ce416af47
Marcos Rezende <marcos.arezende@gmail.com>
2023-12-07 23:13:27 -0300

    feat: adicionada layout botao ativo ao clicar

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
 const html = document.querySelector('html')
 const botoesFoco = document.querySelectorAll('button[data-contexto]')
 const banner = document.querySelector('.app__image')
+const titulo = document.querySelector('.app__title')
 
 botoesFoco.forEach((botao) => {
     botao.onclick = (evento) => {
+        trocarBotaoAtivo(botao)
         alterarContexto(evento.target.getAttribute('data-contexto-id'))
+
     }
 })
+
+function trocarBotaoAtivo(botao) {
+    document.querySelector('button.active').classList.remove('active')
+    botao.classList.add('active')
+}
 
 function alterarContexto(contexto) {
     console.log(contexto)
     html.setAttribute('data-contexto', contexto);
     banner.setAttribute('src', `/imagens/${contexto}.png`)
+    switch (contexto) {
+        case 'foco':
+            titulo.innerHTML = `Otimize sua produtividade,<br>
+            <strong class="app__title-strong">mergulhe no que importa.</strong>`
+            break;
+        case 'descanso-curto':
+            titulo.innerHTML = `Que tal dar uma respirada,<br>
+            <strong class="app__title-strong">faça uma pausa curta.</strong>`
+            break;
+        case 'descanso-longo':
+            titulo.innerHTML = `Hora de voltar à superfície,<br>
+            <strong class="app__title-strong">faça uma pausa longa.</strong>`
+            break;
+        default:
+            break;
+    }
 }
